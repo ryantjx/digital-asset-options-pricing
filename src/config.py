7ad642e62666103b5bea598e2be45fb86e29fdbf
Ryan Jun Xiong Tan <ryan.tanjunxiong@gmail.com>
2025-03-12 14:54:13 +0800

    update settings

---
+++ src/config.py
@@ -0,0 +1,6 @@
+STRIKE_COL = 'strike'
+UNDERLYING_COL = 'underlying'
+RF_RATE_COL = 'rf_rate'
+DIV_COL = 'div'
+SIGMA_COL = 'sigma'
+DAYS_TO_MATURITY_COL = 'days_to_maturity'
---
+++ src/pricer.py
@@ -0,0 +1,15 @@
+import quantlib as ql
+
+"""
+To build:
+1. 
+
+1. BinomialEuropeanPricer
+2. MCEuropeanPricer
+3. BlackScholesPricer
+4. MCEuropeanHestonEngine
+5. FdHestonVanillaEngine
+6. AnalyticPTDHestonEngine
+7. AnalyticEuropeanEngine
+8. FdBlackScholesVanillaEngine
+"""
